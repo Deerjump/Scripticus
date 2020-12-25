4fb5b6213c3d436d603673eee80fba3d35f8bb28
Jed Billman <billman377@gmail.com>
2020-12-25 16:03:11 -0700

    Added contribute command

---
+++ commands/contribute.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+   name: 'contribute',
+   description: 'Seach the Legends of Idleon Wiki!',
+   execute(message, args) {
+      const embed = new MessageEmbed()
+         .setColor('#0000FF')
+         .setTitle('Click here to contribute to Scripticus!')
+         .setURL('https://github.com/Deerjump/Scripticus')
+         .addFields(
+            { name: 'Hey!', value: 'Want to help out development of this bot?\nFollow the link in the title' },
+            { name: 'How to help', value: 'Even if you don\'t know how to code you can still help!\nYou can suggest features through GitHub [here](https://github.com/Deerjump/Scripticus/projects/1 "Suggestions!") as well!' },
+         );
+      message.channel.send(embed);
+   },
+};
