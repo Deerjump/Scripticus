0d91613e23aeb569458774fbfccb37560ffae930
jbillman <billman377@gmail.com>
2020-12-27 14:01:01 -0700

    Minor capitalization changes

--- commands/toolbox.js
+++ commands/toolbox.js
@@ -2,11 +2,11 @@
 const { MessageEmbed } = require('discord.js');
 module.exports = {
    name: 'toolbox',
-   description: 'Provide direct access to the Legends of Idleon Toolbox',
+   description: 'Provides direct access to the Legends of Idleon Toolbox',
    execute(message, args) {
     const embed = new MessageEmbed()
        .setColor('#F9AF9F')
-       .setTitle('Click here to Reach the idleon Toolbox!')
+       .setTitle('Click here to reach the idleon Toolbox!')
        .setURL('https://docs.google.com/spreadsheets/d/1cY3vg1Z-AgOtIcV128D6kSLQRJE_-MUMtyqW0086Zmk/edit?usp=sharing')
        .addFields(
           { name: 'Credit', value: 'Created by Abgreyd and Hateling' },
