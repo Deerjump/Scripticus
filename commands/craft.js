989aaba75de4f29590ae8ddfeb280195d5f6f713
Jed Billman <billman377@gmail.com>
2020-12-28 18:24:19 -0700

    minor style changes

--- commands/craft.js
+++ commands/craft.js
@@ -2,12 +2,12 @@ const craftInfo = require('../util/craftInfo.js');
 const { MessageEmbed } = require('discord.js');
 
 function convertCodeToDisplay(itemCode) {
-	//Returns item's display name from its code name
+	// Returns item's display name from its code name
 	return craftInfo.itemDefinitions[itemCode].replace(/[_|]+/g, ' ');
 }
 
 function convertInputToCode(inputName) {
-	//Converts user input into Title Case, and then into the game's item codename
+	// Converts user input into Title Case, and then into the game's item codename
 	inputName = inputName.toLowerCase().split(' ').map(str => str.charAt(0).toUpperCase() + str.substring(1)).join(' ');
 	for (const itemCode of Object.keys(craftInfo.itemDefinitions)) {
 		if (convertCodeToDisplay(itemCode) === inputName) return itemCode;
@@ -15,30 +15,30 @@ function convertInputToCode(inputName) {
 }
 
 function isRawMaterial(itemCode) {
-	//Checks if item/material is a raw material (aka not crafted - ex: logs, ores, bars, etc.)
+	// Checks if item/material is a raw material (aka not crafted - ex: logs, ores, bars, etc.)
 	return !craftInfo.codes.includes(itemCode);
 }
 
 function isNumeric(str) {
-	//shout out to stack overflow
+	// shout out to stack overflow
 	if (typeof str != "string") return false // we only process strings!  
 	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-		   !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+		   !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
   }
 
 function getItemFullRecipe(itemCode, baseItemQty) {
-	let totalStrArray = []
+	const totalStrArray = [];
 	totalStrArray.push(convertCodeToDisplay(itemCode));
 
 	function generateLine(itemCode, depth = 0, multiplier = 1) {
-		let recipeIndex = craftInfo.codes.indexOf(itemCode);
-		let recipe = craftInfo.costs[recipeIndex];
+		const recipeIndex = craftInfo.codes.indexOf(itemCode);
+		const recipe = craftInfo.costs[recipeIndex];
 
 		for (const itemInfo of recipe) {
 			let [itemCode, itemQty] = itemInfo;
 				itemQty = Number(itemQty);
 
-			let line = `${' '.repeat(depth*3)}- ${convertCodeToDisplay(itemCode)} (x${itemQty*multiplier})`;
+			const line = `${' '.repeat(depth*3)}- ${convertCodeToDisplay(itemCode)} (x${itemQty*multiplier})`;
 			totalStrArray.push(line);
 
 			if (!isRawMaterial(itemCode)) generateLine(itemCode, depth+1, multiplier*itemQty);
@@ -63,7 +63,7 @@ module.exports = {
 		let itemQty = isNumeric(lastArg) ? args[args.length-1] : 1;
 
 		try {
-			//Gets recipe string array, then converts into Markdown code block
+			// Gets recipe string array, then converts into Markdown code block
 			let recipeStrArray = getItemFullRecipe(convertInputToCode(userInput), itemQty);
 			let recipeStr = recipeStrArray.slice(1).join('\n');
 			let title = `Crafting recipe for ${recipeStrArray[0]} (x${itemQty})`;
