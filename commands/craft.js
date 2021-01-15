d4d56f052cef181d1b3062147b36c6dc3be3e559
Deerjump <billman377@gmail.com>
2021-01-14 18:50:05 -0700

    Minor style changes

--- commands/craft.js
+++ commands/craft.js
@@ -8,46 +8,46 @@ function convertCodeToDisplay(itemCode) {
 
 function convertInputToCode(inputName) {
 	// Converts user input into Title Case then returns the item's code name
-	inputName = convertToTitleCase(inputName);
+   inputName = convertToTitleCase(inputName);
 	for (const itemCode of Object.keys(items)) {
-		if (items[itemCode].name === inputName) return itemCode;
-	}
+      if (items[itemCode].name === inputName) return itemCode;
+   }
 }
 
 function convertToTitleCase(str) {
-	//Converts a given string to title case and returns it 
+	// Converts a given string to title case and returns it
 	return str.toLowerCase().split(' ').map(str => str.charAt(0).toUpperCase() + str.substring(1)).join(' ');
 }
 
 function isRawMaterial(itemCode) {
 	// Checks if item/material is a raw material (aka not crafted - ex: logs, ores, bars, etc.), returns boolean
-	return !Object.keys(items[itemCode]).includes('recipe');
+   return !Object.keys(items[itemCode]).includes('recipe');
 }
 
 function isPosInteger(str) {
-	//Checks if a string can be converted into a positive integer, returns boolean
-	if (typeof str != "string") return false // we only process strings!  
-	return !isNaN(str) && 
-		   parseInt(Number(str)) == str && 
+	// Checks if a string can be converted into a positive integer, returns boolean
+	if (typeof str != "string") return false; // we only process strings!
+	return !isNaN(str) &&
+		   parseInt(Number(str)) == str &&
 		   !isNaN(parseInt(str, 10)) &&
 		   parseInt(str, 10) > 0;
 }
 
-function generateRecipe(itemCode, totalRecipe={}) {
-	//Input: item code (e.g. EquipmentHats8, MaxCapBagB5, etc.)
-	//1. Get recipe for given itemCode
-	//2. For each item in the recipe, create an object with information
-		//a. If item is not raw, get its recipe using its item code and add to object
-		//b. Repeat starting from step 1
-	//3. Add each item to 'totalRecipe'
-	//Returns a nested object
+function generateRecipe(itemCode, totalRecipe = {}) {
+	// Input: item code (e.g. EquipmentHats8, MaxCapBagB5, etc.)
+	// 1. Get recipe for given itemCode
+	// 2. For each item in the recipe, create an object with information
+		// a. If item is not raw, get its recipe using its item code and add to object
+		// b. Repeat starting from step 1
+	// 3. Add each item to 'totalRecipe'
+   // Returns a nested object
 	const recipe = items[itemCode].recipe.costs;
 
 	for (let i = 0; i < recipe.length; i++) {
-		let itemCode = recipe[i][0], itemQty = recipe[i][1].replace(' ', ''); //for some reason item quantities in each cost array has a leading space in it
+		let itemCode = recipe[i][0], itemQty = recipe[i][1].replace(' ', ''); // for some reason item quantities in each cost array has a leading space in it
 		let itemObj = {
 			isRaw: isRawMaterial(itemCode),
-			qty: itemQty
+			qty: itemQty,
 		};
 		if (!itemObj.isRaw) {
 			itemObj.recipe = generateRecipe(itemCode);
@@ -55,23 +55,23 @@ function generateRecipe(itemCode, totalRecipe={}) {
 		totalRecipe[itemCode] = itemObj;
 	}
 
-	return totalRecipe
+	return totalRecipe;
 }
 
-function generateRecipeText(recipe, depth=0, multiplier=1, recipeTextArr = []) {
-	//Print raw materials first
-	//Print craftables second
+function generateRecipeText(recipe, depth = 0, multiplier = 1, recipeTextArr = []) {
+	// Print raw materials first
+	// Print craftables second
 	for (const itemCode in recipe) {
 		if (recipe[itemCode].isRaw) {
-			let line = `${' '.repeat(depth*3)}- ${convertCodeToDisplay(itemCode)} (x${recipe[itemCode].qty * multiplier})`;
+			let line = `${' '.repeat(depth * 3)}- ${convertCodeToDisplay(itemCode)} (x${recipe[itemCode].qty * multiplier})`;
 			recipeTextArr.push(line);
 		}
 	}
 	for (const itemCode in recipe) {
 		if (!recipe[itemCode].isRaw) {
-			let line = `${' '.repeat(depth*3)}- ${convertCodeToDisplay(itemCode)} (x${recipe[itemCode].qty * multiplier})`;
+			let line = `${' '.repeat(depth * 3)}- ${convertCodeToDisplay(itemCode)} (x${recipe[itemCode].qty * multiplier})`;
 			recipeTextArr.push(line);
-			generateRecipeText(recipe[itemCode].recipe, depth+1, multiplier*recipe[itemCode].qty, recipeTextArr);
+			generateRecipeText(recipe[itemCode].recipe, depth + 1, multiplier * recipe[itemCode].qty, recipeTextArr);
 		}
 	}
 	return recipeTextArr;
@@ -84,13 +84,13 @@ module.exports = {
 	usage: '<item name> <item quantity>',
 	execute(message, args) {
 		if (!args.length) {
-			return message.channel.send('You must provide an item: !craft <item name> <item quantity>')
-		}
-		
-		let lastArg = args[args.length-1];
+			return message.channel.send('You must provide an item: !craft <item name> <item quantity>');
+      }
+
+		let lastArg = args[args.length - 1];
 		let userInput = isPosInteger(lastArg) ? args.slice(0, -1).join(' ') : args.join(' ');
-		let itemQty = isPosInteger(lastArg) ? args[args.length-1] : 1;
-		
+      let itemQty = isPosInteger(lastArg) ? args[args.length - 1] : 1;
+
 		try {
 			let recipeObj = generateRecipe(convertInputToCode(userInput));
 			let recipeText = generateRecipeText(recipeObj, 0, itemQty).join('\n');
@@ -106,7 +106,5 @@ module.exports = {
 				.setDescription('Invalid item, please try again! Check the [wiki](https://idleon.miraheze.org/wiki/Smithing) for a list of all craftable items!');
 			message.channel.send(embed);
 		}
-
-	}
-
-}
+   },
+};
