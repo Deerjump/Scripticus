0b7a8da4587842cfd6fea5c54d25556bc1abffc6
Kevin Lee <kevinlee3627@gmail.com>
2021-01-15 19:13:58 -0600

    Added ability to toggle embed view between full recipe and total materials

--- commands/craft.js
+++ commands/craft.js
@@ -8,10 +8,10 @@ function convertCodeToDisplay(itemCode) {
 
 function convertInputToCode(inputName) {
 	// Converts user input into Title Case then returns the item's code name
-   inputName = convertToTitleCase(inputName);
+   	inputName = convertToTitleCase(inputName);
 	for (const itemCode of Object.keys(items)) {
-      if (items[itemCode].name === inputName) return itemCode;
-   }
+    	if (items[itemCode].name === inputName) return itemCode;
+   	}
 }
 
 function convertToTitleCase(str) {
@@ -34,18 +34,12 @@ function isPosInteger(str) {
 }
 
 function generateRecipe(itemCode, totalRecipe = {}) {
-	// Input: item code (e.g. EquipmentHats8, MaxCapBagB5, etc.)
-	// 1. Get recipe for given itemCode
-	// 2. For each item in the recipe, create an object with information
-		// a. If item is not raw, get its recipe using its item code and add to object
-		// b. Repeat starting from step 1
-	// 3. Add each item to 'totalRecipe'
-   // Returns a nested object
 	const recipe = items[itemCode].recipe.costs;
 
 	for (let i = 0; i < recipe.length; i++) {
-		let itemCode = recipe[i][0], itemQty = recipe[i][1].replace(' ', ''); // for some reason item quantities in each cost array has a leading space in it
-		let itemObj = {
+		//For some reason item quantities in each cost array has a leading space in it
+		const itemCode = recipe[i][0], itemQty = recipe[i][1].replace(' ', ''); 
+		const itemObj = {
 			isRaw: isRawMaterial(itemCode),
 			qty: itemQty,
 			name: convertCodeToDisplay(itemCode)
@@ -55,20 +49,20 @@ function generateRecipe(itemCode, totalRecipe = {}) {
 			itemObj.recipe = generateRecipe(itemCode);
 		}	
 	}
-	return totalRecipe
+	return totalRecipe;
 }
 
 function generateRecipeText(recipe, depth=0, multiplier=1, recipeTextArr = []) {
 	//Sort recipe object so raw materials appear first
-	let sorted = Object.entries(recipe).sort(([, {isRaw: a}], [, {isRaw: b}]) => b - a);
+	const sorted = Object.entries(recipe).sort(([, {isRaw: a}], [, {isRaw: b}]) => b - a);
 	for (let i = 0; i < sorted.length; i++) {
-		let item = sorted[i][1];
-		let line = `${' '.repeat(depth*3)}- ${item.name} (x${item.qty * multiplier})`;
+		const item = sorted[i][1];
+		const line = `${' '.repeat(depth*3)}- ${item.name} (x${item.qty * multiplier})`;
 		recipeTextArr.push(line);
 
-		if(!item.isRaw) generateRecipeText(item.recipe, depth+1, multiplier*item.qty, recipeTextArr)
+		if(!item.isRaw) generateRecipeText(item.recipe, depth+1, multiplier*item.qty, recipeTextArr);
 	}
-	return recipeTextArr.join('\n')
+	return recipeTextArr.join('\n');
 
 }
 
@@ -78,14 +72,27 @@ function generateTotalMaterials(recipeObj, multiplier=1, totalMaterials={}) {
 			if (totalMaterials[itemCode] == undefined) totalMaterials[itemCode] = 0;
 			totalMaterials[itemCode] += recipeObj[itemCode].qty * multiplier;	 
 		} else {
-			generateTotalMaterials(recipeObj[itemCode].recipe, multiplier*recipeObj[itemCode].qty, totalMaterials)
+			generateTotalMaterials(recipeObj[itemCode].recipe, multiplier*recipeObj[itemCode].qty, totalMaterials);
 		}
 	}
-	return totalMaterials
+	return totalMaterials;
 }
 
 function generateTotalMaterialsText(materialsObj) {
 	return Object.entries(materialsObj).map(([itemCode, qty]) => `- ${convertCodeToDisplay(itemCode)} (x${qty})`).join('\n');
+}
+
+/*-----------------------------------------------------*/
+function createEmbed(title, description, footer) {
+	return new MessageEmbed()
+		.setTitle(title)
+		.setDescription(description)
+		.setFooter(footer);
+}
+
+function editEmbed(embedInstance, title, description, footer) {
+	const newEmbed = createEmbed(title, '```'+description+'```', footer);
+	embedInstance.edit(newEmbed);
 }
 
 
@@ -97,41 +104,51 @@ module.exports = {
 
 		if (!args.length) {
 			return message.channel.send('You must provide an item: !craft <item name> <item quantity>');
-      }
+      	}
 
-		let lastArg = args[args.length - 1];
-		let userInput = isPosInteger(lastArg) ? args.slice(0, -1).join(' ') : args.join(' ');
-      let itemQty = isPosInteger(lastArg) ? args[args.length - 1] : 1;
+		const lastArg = args[args.length - 1];
+		const userInput = isPosInteger(lastArg) ? args.slice(0, -1).join(' ') : args.join(' ');
+      	const itemQty = isPosInteger(lastArg) ? args[args.length - 1] : 1;
 
 		try {
-			let recipeObj = generateRecipe(convertInputToCode(userInput));
-			let recipeText = generateRecipeText(recipeObj, 0, itemQty);
+			const recipeObj = generateRecipe(convertInputToCode(userInput));
+			const recipeTitle = `Crafting recipe for ${convertToTitleCase(userInput)} (x${itemQty})`;
+			const recipeText = generateRecipeText(recipeObj, 0, itemQty);
+			const recipeFooter = `To see total material costs, react with 👎`;
 
-			let materialsObj = generateTotalMaterials(recipeObj);
-			let materialsText = generateTotalMaterialsText(materialsObj)
+			const materialsObj = generateTotalMaterials(recipeObj);
+			const materialsTitle = `Total material costs for ${convertToTitleCase(userInput)} (x${itemQty})`;
+			const materialsText = generateTotalMaterialsText(materialsObj);
+			const materialsFooter = `To see full recipe, react with 👍`;
 
-			let title = `Crafting recipe for ${convertToTitleCase(userInput)} (x${itemQty})`;
-			let description = '```\n' + recipeText + '```';
-			const embed = new MessageEmbed()
-				.setTitle(title)
-				.setDescription(description)
-				.setFooter('To see total material costs, react with 👍')
+			const initialEmbed = createEmbed(recipeTitle, '```'+recipeText+'```', recipeFooter)
 
-			message.channel.send(embed).then(sentEmbed => {
+			message.channel.send(initialEmbed).then(sentEmbed => {
 				sentEmbed.react('👍');
+				sentEmbed.react('👎');
 
-				const filter = (reaction, user) => ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id
-				sentEmbed.awaitReactions(filter, {max: 1})
-					.then(collected => {
-						const newEmbed = new MessageEmbed(sentEmbed.embeds[0]);
-						newEmbed.setDescription('```'+materialsText+'```');
-						sentEmbed.edit(newEmbed);
-					})
+				const filter = (reaction, user) => ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+				const collectorLifespan = 10000;
+				const collector = sentEmbed.createReactionCollector(filter, {time: collectorLifespan});
+				collector.on('collect', (reaction, user) => {
+					if (reaction.emoji.name === '👍') {
+						editEmbed(sentEmbed, recipeTitle, recipeText, recipeFooter);
+					} else if (reaction.emoji.name === '👎') {
+						editEmbed(sentEmbed, materialsTitle, materialsText, materialsFooter);
+					}
+				})
+
+				collector.on('end', collected => {
+					let expiredEmbed = createEmbed(sentEmbed.embeds[0].title, sentEmbed.embeds[0].description, 'Message expiration message');
+					sentEmbed.edit(expiredEmbed);
+				})
+
 			})
-		} catch {
-			let embed = new MessageEmbed()
+		} catch(err) {
+			const embed = new MessageEmbed()
 				.setDescription('Invalid item, please try again! Check the [wiki](https://idleon.miraheze.org/wiki/Smithing) for a list of all craftable items!');
 			message.channel.send(embed);
+			console.log(err);
 		}
    },
 };
