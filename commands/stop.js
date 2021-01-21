5e9c8edc7b3c61a492aca3274ab101feca405863
Kevin Lee <kevinlee3627@gmail.com>
2021-01-21 15:44:07 -0600

    Installed pm2, started stop command

---
+++ commands/stop.js
@@ -0,0 +1,10 @@
+const { MessageEmbed } = require('discord.js');
+const { MongoClient } = require('mongodb')
+
+module.exports = {
+  name: 'stop',
+  description: 'Kills the bot!',
+  execute(message) {
+
+  },
+};
