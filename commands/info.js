b5dfc20691de5903069dadcf434faa8fc459f368
Deerjump <billman377@gmail.com>
2021-01-23 17:59:57 -0700

    Started 'info' command

---
+++ commands/info.js
@@ -0,0 +1,54 @@
+const { MessageEmbed } = require('discord.js');
+const itemList = require('./../util/items.js');
+const monsterList = require('./../util/monsters.js');
+
+module.exports = {
+  name: 'info',
+  description: 'Find information on monsters or items!',
+  usage: '<item or monster name>',
+  args: true,
+  cooldown: 1,
+  execute(message, args) {
+    const lowerCaseArgs = args.join(' ').toLowerCase();
+    console.log(lowerCaseArgs);
+    const item = getItem(lowerCaseArgs);
+    let itemDesc;
+    if (item) {
+      console.log(item);
+      itemDesc = `Name: ${item.name}, Sell Price: ${item.sellPrice}, type: ${item.type}, Level Required: ${item.lvReqToEquip}`;
+    }
+    const monster = getMonster(lowerCaseArgs);
+
+    let monstDesc;
+    if (monster) {
+      console.log(monster);
+      monstDesc = `Name: ${monster.Name.replace('_', ' ')}, Activity: ${monster.AFKtype}, HP: ${monster.MonsterHPTotal}, Defence: ${monster.Defence}`;
+    }
+    if(!item && !monster) {
+      return message.reply('That item/monster doesn\'t exist!');
+    }
+    const response = new MessageEmbed().setDescription(monstDesc || itemDesc);
+    message.reply(response);
+  },
+};
+
+function getMonster(name) {
+  let toReturn;
+  for (const [, value] of Object.entries(monsterList)) {
+    if (value.Name && value.Name.toLowerCase().replace('_', ' ') === name) {
+      toReturn = value;
+    }
+  }
+  return toReturn;
+}
+
+function getItem(name) {
+  let toReturn;
+  for (const [, value] of Object.entries(itemList)) {
+    if (value.name && value.name.toLowerCase() === name) {
+      toReturn = value;
+    }
+  }
+
+  return toReturn;
+}
