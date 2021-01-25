cb34f92687de1d74385b5632530e8f88347e4273
Kevin Lee <kevinlee3627@gmail.com>
2021-01-25 15:34:23 -0600

    Changed execute function in !restart to be async

--- commands/restart.js
+++ commands/restart.js
@@ -4,18 +4,16 @@ module.exports = {
   name: 'restart',
   description: 'Provides direct access to the Legends of Idleon Toolbox',
   whitelist: ['90598254688874496'],
-  execute(message) {
+  async execute(message) {
     if (this.whitelist.includes(message.author.id)) {
       message.channel.send('Restarting bot!');
-      MongoConnection.disconnect()
-        .then(() => {
-          console.log('MongoConnection disconnected');
-          message.client.commands
-            .filter(command => command.stop != null)
-            .forEach(command => command.stop());
-          process.exit(0);
-        })
+      await MongoConnection.disconnect()
+        .then(console.log('MongoConnection disconnected'))
         .catch(err => console.error(err));
+      message.client.commands
+        .filter(command => command.stop != null)
+        .forEach(command => command.stop());
+      process.exit(0);
     } else {
       message.channel.send('You cannot use this command!');
     }
