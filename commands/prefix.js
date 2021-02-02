7d79f22cfca29bd0ad07603eb155de26426cd2c2
Kevin Lee <kevinlee3627@gmail.com>
2021-02-01 23:26:28 -0600

    Added usage field to prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'prefix',
   description: 'Changes prefix for current server',
   args: true,
+  usage: '<new prefix>',
   async execute(message, args) {
     const newPrefix = args[0];
     if (args[0] && args[0].length === 0) return message.channel.send('Prefix must be at least 1 character!');
