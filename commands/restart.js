cf0a8fc88d03062b7b43d02921b8a4f840204cfa
Deerjump <billman377@gmail.com>
2021-02-16 21:59:00 -0700

    Fixed description of restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,7 @@ const { MongoConnection } = require('../mongo/connection.js');
 
 module.exports = {
   name: 'restart',
-  description: 'Provides direct access to the Legends of Idleon Toolbox',
+  description: 'Restarts the bot!',
   whitelist: ['90598254688874496', '191085842469486592'],
   async execute(message) {
     if (this.whitelist.includes(message.author.id)) {
