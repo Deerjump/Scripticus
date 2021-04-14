277cff0da1324eba6899b20047871ce7d3a7b9a8
Deerjump <billman377@gmail.com>
2021-04-13 20:17:31 -0600

    Updated formulas to reflect originals exactly

--- util/formulas.js
+++ util/formulas.js
@@ -1,17 +1,17 @@
 module.exports = {
-  add: (a, b, c) =>
-    c != 0 ? (((b + c) / c + 0.5 * (a - 1)) / (b / c)) * a * b : b * a,
-  addlower: (a, b, c) => b + c * (a + 1),
-  decay: (a, b, c) => (c * a) / (c + b),
-  decaylower: (a, b, c) => (b * (a + 1)) / (a + 1 + c) - (b * a) / (a + c),
-  decaymulti: (a, b, c) => 1 + (a * b) / (a + c),
-  decaymultilower: (a, b, c) => (b * (a + 1)) / (a + 1 + c) - (b * a) / (a + c),
-  bigbase: (a, b, c) => b + c * a,
-  bigbaselower: (a, b, c) => c,
-  intervaladd: (a, b, c) => b + Math.floor(a / c),
-  intervaladdlower: (a, b, c) =>
-    Math.max(Math.floor((a + 1) / c), 0) - Math.max(Math.floor(a / c), 0),
-  reduce: (a, b, c) => b - c * a,
-  reducelower: (a, b, c) => b - c * (a + 1),
+  add: (b, c, e) =>
+    c != 0 ? (((b + c) / c + 0.5 * (e - 1)) / (b / c)) * e * b : b * e,
+  addlower: (b, c, e) => b + c * (e + 1),
+  decay: (b, c, e) => (b * e) / (e + c),
+  decaylower: (b, c, e) => (b * (e + 1)) / (e + 1 + c) - (b * e) / (e + c),
+  decaymulti: (b, c, e) => 1 + (e * b) / (e + c),
+  decaymultilower: (b, c, e) => (b * (e + 1)) / (e + 1 + c) - (b * e) / (e + c),
+  bigbase: (b, c, e) => b + c * e,
+  bigbaselower: (b, c, e) => c,
+  intervaladd: (b, c, e) => b + Math.floor(e / c),
+  intervaladdlower: (b, c, e) =>
+    Math.max(Math.floor((e + 1) / c), 0) - Math.max(Math.floor(e / c), 0),
+  reduce: (b, c, e) => b - c * e,
+  reducelower: (b, c, e) => b - c * (e + 1),
   log: (num) => Math.log(Math.max(num, 1)) / 2.303,
 };
