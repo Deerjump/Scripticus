3bd7d1fc6464cd94a2c38d15c97eac89bd98739d
Deerjump <billman377@gmail.com>
2021-09-06 16:40:09 -0600

    Updated expiration message

--- commands/craft.js
+++ commands/craft.js
@@ -123,7 +123,7 @@ function generateMaterialsEmbed({ itemName, recipe, amount }) {
 }
 
 function timeoutMessage(message) {
-  const timeoutMsg = '❌ Message has expired!';
+  const timeoutMsg = '❌ Timed out';
   const embed = message.embeds[0];
 
   embed.setFooter(timeoutMsg);
@@ -178,7 +178,7 @@ module.exports = {
 
     const collector = sentEmbed.createMessageComponentCollector({
       filter,
-      time: 30000,
+      time: 60000,
     });
 
     collector.on('collect', async (interaction) => {
