aaf7df8ccd66c7d81a6465a51e4ebc5c291b3b43
jbillman <billman377@gmail.com>
2022-03-07 23:30:40 -0700

    Fixed command base classes

--- src/commands/commandClasses.ts
+++ src/commands/commandClasses.ts
@@ -6,24 +6,24 @@ import {
   MessageContextMenuInteraction,
   UserContextMenuInteraction,
 } from 'discord.js';
-import { ApplicationCommandTypes } from 'discord.js/typings/enums';
+import { ApplicationCommandType } from 'discord.js/node_modules/discord-api-types';
 
 export abstract class ApplicationCommand {
-  protected abstract type: ApplicationCommandTypes;
+  private readonly MAX_NAME_LENGTH = 32;
   protected defaultPermission = true;
   abstract commandBuilder: SlashCommandBuilder | ContextMenuCommandBuilder;
-  name: string;
   roleRequired: string = 'EVERYONE';
   global = false;
+  name: string;
 
   constructor(name: string) {
-    // https://discord.com/developers/docs/interactions/application-commands#application-commands
-    const regex = /^[[a-z0-9_\]-]{1,32}$/;
-    if (!regex.test(name)) {
-      throw new TypeError(
-        '"name" must be all lower case without spaces and less than 32 characters long'
-      );
+    if (!name || name === '') {
+      throw new TypeError(`"name" field cannot be null or empty!`);
     }
+    if (name.length > this.MAX_NAME_LENGTH) {
+      throw new RangeError(`"name" field must be less than ${this.MAX_NAME_LENGTH} characters`);
+    }
+
     this.name = name;
   }
 
@@ -31,16 +31,24 @@ export abstract class ApplicationCommand {
 }
 
 export abstract class SlashCommand extends ApplicationCommand {
-  type: ApplicationCommandTypes.CHAT_INPUT = ApplicationCommandTypes.CHAT_INPUT;
+  type: ApplicationCommandType.ChatInput = ApplicationCommandType.ChatInput;
   commandBuilder = new SlashCommandBuilder();
   description: string;
 
   constructor(name: string, description: string) {
     super(name);
-    if (description.length < 1 || description.length > 100)
+    const nameRegex = /^[[a-z0-9_\]-]*$/;
+
+    if (!nameRegex.test(name)) {
+      throw new TypeError(`"name" must be all lower case without spaces. Provided: '${name}'`);
+    }
+
+    if (description.length < 1 || description.length > 100) {
       throw new TypeError(
         `"description" field must be betweeen 1 and 100 characters! Provided ${description.length}`
       );
+    }
+
     this.description = description;
     this.commandBuilder
       .setName(this.name)
@@ -52,25 +60,29 @@ export abstract class SlashCommand extends ApplicationCommand {
 }
 
 export abstract class ContextMenuCommand extends ApplicationCommand {
-  abstract type: Exclude<ApplicationCommandTypes, ApplicationCommandTypes.CHAT_INPUT>;
   commandBuilder = new ContextMenuCommandBuilder();
 
   constructor(name: string) {
     super(name);
     this.commandBuilder.setName(this.name).setDefaultPermission(this.defaultPermission);
-  } 
+  }
 
   abstract handleInteract(interaction: ContextMenuInteraction): void;
 }
 
 export abstract class MessageCommand extends ContextMenuCommand {
-  type: ApplicationCommandTypes.MESSAGE = ApplicationCommandTypes.MESSAGE;
+  type: ApplicationCommandType.Message = ApplicationCommandType.Message;
+
+  constructor(name: string) {
+    super(name);
+    this.commandBuilder.setType(this.type);
+  }
 
   abstract handleInteract(interaction: MessageContextMenuInteraction): void;
 }
 
 export abstract class UserCommand extends ContextMenuCommand {
-  type: ApplicationCommandTypes.USER = ApplicationCommandTypes.USER;
+  type: ApplicationCommandType.User = ApplicationCommandType.User;
 
   abstract handleInteract(interaction: UserContextMenuInteraction): void;
 }
